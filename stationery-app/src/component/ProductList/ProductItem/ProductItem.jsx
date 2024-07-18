120ec173dea5e1ab9176ba333516aec4bb20f657
lapnguyentiti <113778401+lapnguyen331@users.noreply.github.com>
2024-07-18 19:15:27 +0700

    Update ProductItem.jsx

--- stationery-app/src/component/ProductList/ProductItem/ProductItem.jsx
+++ stationery-app/src/component/ProductList/ProductItem/ProductItem.jsx
@@ -22,7 +22,7 @@ function ProductItem({ productid }) {
                 <h3 className="titleProduct">
                     <Link to={PATH.USER.PRODUCTDETAIL.replace(':productId',product.id)}>{product.title}</Link>
                     </h3>
-            </div>
+{/*             </div> */}
 {/*             <div className="card_bottom"> */}
                 <div className="price_container">
                                 <span className="prices">
