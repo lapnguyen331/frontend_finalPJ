e1252e0f549eb18616a5a174af9e61cea40a51bd
lapnguyentiti <113778401+lapnguyen331@users.noreply.github.com>
2024-07-18 19:23:26 +0700

    Update index.js

--- stationery-app/src/index.js
+++ stationery-app/src/index.js
@@ -4,7 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createTheme, ThemeProvider} from "@mui/material";
-
+import {BrowserRouter} from "react-router-dom"
+import RouterCustom from './router';
+import './style/style.scss'
 const theme = createTheme({
     root: {
 
