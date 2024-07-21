c0898e6d74d25a4fd934c33eca3ad481d6bbd8d9
PhanTTDuong <21130331@st.hcmuaf.edu.vn>
2024-07-21 22:04:18 +0700

    sua phan productdescript

--- stationery-app/src/component/ProductsDetail/ProductDetailsDescription/ProductDiscription.jsx
+++ stationery-app/src/component/ProductsDetail/ProductDetailsDescription/ProductDiscription.jsx
@@ -1,16 +1,19 @@
 import productsData from "../../../data/Product/productData";
 import './ProductDiscriptionStyles.scss'
-function ProductDiscription({ productId }) {
+
+function ProductDiscription({productId}) {
     const product = productsData.find(item => item.id === productId);
 
     const name = product.name;
     const descript = product.descript;
-    const characteristic = product.characteristic.split('\n\n-').map((item, index) => (
-        <span key={index}>{item.trim() ? `- ${item.trim()}` : ''}<br /></span>
-    ));
-    const improvement = product.improvement.split('\n\n-').map((item, index) => (
-        <span key={index}>{item.trim() ? `- ${item.trim()}` : ''}<br /></span>
-    ));
+    const characteristic =  product.characteristic
+        ? product.characteristic.split('\n\n-').map((item, index) => (
+        <span key={index}>{item.trim() ? `- ${item.trim()}` : ''}<br/></span>
+    )) : null;
+    const improvement = product.improvement
+        ? product.improvement.split('\n\n-').map((item, index) => (
+        <span key={index}>{item.trim() ? `- ${item.trim()}` : ''}<br/></span>
+    )) : null;
 
     return (
         <div className="description_container">
@@ -20,14 +23,18 @@ function ProductDiscription({ productId }) {
                     <h4 className="label"> Tên sản phẩm: </h4>
                     <span>  {name}</span>
                 </div>
-                <div className="characteristic_p">
-                    <h4 className="label">Đặc điểm sản phẩm: </h4>
-                    <span> {characteristic}</span>
-                </div>
-                <div className="improvement_p">
-                    <h4 className="label"> Cải tiến: </h4>
-                    <span> {improvement}</span>
-                </div>
+                {product.category !== 'books' && (
+                    <>
+                        <div className="characteristic_p">
+                            <h4 className="label">Đặc điểm sản phẩm: </h4>
+                            <span> {characteristic}</span>
+                        </div>
+                        <div className="improvement_p">
+                            <h4 className="label"> Cải tiến: </h4>
+                            <span> {improvement}</span>
+                        </div>
+                    </>
+                )}
                 <p>{descript}</p>
             </div>
 
